51d3d8ade2eb2f00011dd51c1bc386a8bc1cadf7
CarlosPossi <rm558758@fiap.com.br>
2026-03-14 22:42:18 -0300

    feat: criação do controller de Medico

---
+++ src/main/java/br/com/java/API_consultas/controller/MedicoController.java
@@ -0,0 +1,32 @@
+package br.com.java.API_consultas.controller;
+import br.com.java.API_consultas.model.Medico;
+import br.com.java.API_consultas.service.MedicoService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/medicos")
+@CrossOrigin
+
+public class MedicoController {
+    private final MedicoService service;
+
+    public MedicoController(MedicoService service){this.service = service;}
+
+    @PostMapping
+    public Medico criar(@RequestBody Medico medico){return service.salvar(medico);}
+
+    @GetMapping
+    public List<Medico> listar() {return service.listar();}
+
+    @GetMapping("/{id}")
+    public Medico buscarPorId(@PathVariable Long id){return service.buscarPorId(id);}
+
+    @PutMapping("/{id}")
+    public Medico atualizar(@PathVariable Long id, @RequestBody Medico medico){
+        return service.atualizar(id, medico);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id){service.deletar(id);}
+}
