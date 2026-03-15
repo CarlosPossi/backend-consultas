54697263c1617a5b2d53306b6696cc1ffbba3326
CarlosPossi <rm558758@fiap.com.br>
2026-03-14 22:43:39 -0300

    feat: criação do repository de Medico

--- src/main/java/br/com/java/API_consultas/repository/MedicoRepository.java
+++ src/main/java/br/com/java/API_consultas/repository/MedicoRepository.java
@@ -1,4 +1,6 @@
 package br.com.java.API_consultas.repository;
+import br.com.java.API_consultas.model.Medico;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MedicoRepository {
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
 }
